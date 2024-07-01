7a3186046872b7cdeff16f0ded24f4fb1fa402cd
zaryabzubair <zaryabzubair.m@gmail.com>
2024-07-01 23:06:21 +0500

    update files

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=w53+lsj&z#x**mmw7h6wu%l%s-x959virwa_)r$+o)r7v20wy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost',]
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost',]
 
 
 # Application definition
