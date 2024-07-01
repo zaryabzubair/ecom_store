0739223983b247a781a1716d8614b1cf56896feb
zaryabzubair <zaryabzubair.m@gmail.com>
2024-07-01 23:16:52 +0500

    update files

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "Building project packages..."
-python3.9 -m pip install --upgrade pip
-python3.9 pip install -r requirements.txt
+pip install -r requirements.txt
 
 echo "Collecting static files..."
 python3.9 manage.py collectstatic --noinput
